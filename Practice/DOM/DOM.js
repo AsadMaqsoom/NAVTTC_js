6cc0061346adb9d2e79521ac55e5f440ca18c56e
Asad Maqsoom <166494934+AsadMaqsoom@users.noreply.github.com>
2024-04-11 16:31:39 +0000

    update DOM by me

--- Practice/DOM/DOM.js
+++ Practice/DOM/DOM.js
@@ -39,29 +39,45 @@
 // // Use getElementById to retrieve the input element.
 // // Validate the input (e.g., check if it's not empty) when the submit button is clicked.
 
+// const form = document.querySelector('form');
+// // // this usecase will give you empty
+// // // const height = parseInt(document.querySelector('#height').value)
+
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+
+//   const height = parseInt(document.querySelector('#height').value);
+//   const weight = parseInt(document.querySelector('#weight').value);
+//   const results = document.querySelector('#results');
+
+//   if (height === '' || height < 0 || isNaN(height)) {
+//     results.innerHTML = `Please give a valid height ${height}`;
+//   } else if (weight === '' || weight < 0 || isNaN(weight)) {
+//     results.innerHTML = `Please give a valid weight ${weight}`;
+//   } else {
+//     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+//     //show the result
+//     results.innerHTML = `<span>${bmi}</span>`;
+//   }
+// });
 const form = document.querySelector('form');
-// // this usecase will give you empty
-// // const height = parseInt(document.querySelector('#height').value)
-
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  const height = parseInt(document.querySelector('#height').value);
-  const weight = parseInt(document.querySelector('#weight').value);
-  const results = document.querySelector('#results');
-
-  if (height === '' || height < 0 || isNaN(height)) {
-    results.innerHTML = `Please give a valid height ${height}`;
-  } else if (weight === '' || weight < 0 || isNaN(weight)) {
-    results.innerHTML = `Please give a valid weight ${weight}`;
-  } else {
+form.addEventListener('submit',function(bmi){
+  bmi.preventDefault();
+  const height = parseInt(document.querySelector('#height').value)
+  const weight = parseInt(document.querySelector('#weight').value)
+  const results = document.querySelector('#results')
+  if (height === '' || height <0 || isNaN(height)){
+    results.innerHTML = `Please give a Valid height ${height}`
+  } 
+  else if (weight === '' || weight <0 || isNaN(weight)) {
+    results.innerHTML = `Please give a Valid weight ${weight}`
+  }
+  else{
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    //show the result
     results.innerHTML = `<span>${bmi}</span>`;
   }
-});
-let a = 2;
-let b = 3;
-console.log(a+b)
+ 
+})
+
 
 
