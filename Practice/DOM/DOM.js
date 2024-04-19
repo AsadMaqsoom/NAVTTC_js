b1b34fe717f0f5767997cda045c2e85233c0cde9
Asad Maqsoom <166494934+AsadMaqsoom@users.noreply.github.com>
2024-04-19 14:09:44 +0000

    update DOM files

--- Practice/DOM/DOM.js
+++ Practice/DOM/DOM.js
@@ -60,24 +60,33 @@
 //     results.innerHTML = `<span>${bmi}</span>`;
 //   }
 // });
-const form = document.querySelector('form');
-form.addEventListener('submit',function(bmi){
-  bmi.preventDefault();
-  const height = parseInt(document.querySelector('#height').value)
-  const weight = parseInt(document.querySelector('#weight').value)
-  const results = document.querySelector('#results')
-  if (height === '' || height <0 || isNaN(height)){
-    results.innerHTML = `Please give a Valid height ${height}`
-  } 
-  else if (weight === '' || weight <0 || isNaN(weight)) {
-    results.innerHTML = `Please give a Valid weight ${weight}`
-  }
-  else{
-    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    results.innerHTML = `<span>${bmi}</span>`;
-  }
+
+
+// const form = document.querySelector('form');
+// form.addEventListener('submit',function(bmi){
+//   bmi.preventDefault();
+//   const height = parseInt(document.querySelector('#height').value)
+//   const weight = parseInt(document.querySelector('#weight').value)
+//   const results = document.querySelector('#results')
+//   if (height === '' || height <0 || isNaN(height)){
+//     results.innerHTML = `Please give a Valid height ${height}`
+//   } 
+//   else if (weight === '' || weight <0 || isNaN(weight)) {
+//     results.innerHTML = `Please give a Valid weight ${weight}`
+//   }
+//   else{
+//     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+//     results.innerHTML = `<span>${bmi}</span>`;
+//   }
  
-})
+// })
+
+// Event Practice
+function click(message) {
+  // document.write('Hi Asad')
+  alert(message)
+}
+
 
 
 
